cd5c60d7c9858d3b30559a110718059936941656
David Santamaria Lago <dsantamarialago@stubhub.com>
2018-07-16 01:18:54 +0200

    created dummy consumer for contract testing provider

---
+++ src/main/java/com/contract/consumer/NavesController.java
@@ -0,0 +1,23 @@
+package com.contract.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class NavesController {
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
+    private static final String STARSHIPS_ENDPOINT = "http://localhost:8080/starship?id=";
+
+    @GetMapping("/nave")
+    public Object getNave(@RequestParam(value = "id") Long id) {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity responseEntity = restTemplate.getForEntity(STARSHIPS_ENDPOINT + id.toString(), String.class);
+        return responseEntity.getBody();
+    }
+}
